9082bc611366046f05107b7b76e1a4c1a4d11831
Abigail Ng <abigailjng@gmail.com>
2023-02-16 14:32:40 -0500

    Add props

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,44 @@
 import React, { useEffect } from 'react'
 
+const noop = () => { }
+
 type ChariotConnectProps = {
-    cid: string
+    cid: string,
+    theme?: string | Object,
+    onDonationRequest: () => void,
+    onSuccess?: (e: any) => void,
+    onExit?: (e: any) => void,
 }
+
 const ChariotConnect: React.FC<ChariotConnectProps> = ({
-    cid
+    cid,
+    theme = "DefaultTheme",
+    onDonationRequest,
+    onSuccess = noop,
+    onExit = noop,
 }) => {
     useEffect(() => {
         const script = document.createElement('script');
 
-        script.src = "https://cdn.givechariot.com/chariot-connect.umd.js";
+        script.src = "http://127.0.0.1:5173/dist/chariot-connect.umd.js";
         script.async = true;
 
         document.body.appendChild(script);
 
         const connect = document.createElement('chariot-connect') as any;
         connect.setAttribute('cid', cid);
+        connect.setAttribute('theme', (typeof theme === "string") ? theme : 'customTheme')
+
+        connect.addEventListener("CHARIOT_INIT", () => {
+            connect.onDonationRequest(onDonationRequest);
+            connect.registerTheme("customTheme", theme);
+        });
+
+        connect.addEventListener("CHARIOT_SUCCESS", onSuccess)
+        connect.addEventListener("CHARIOT_EXIT", onExit)
 
         const connectContainer = document.getElementById('connectContainer');
         connectContainer?.appendChild(connect)
-
-        const loadConnect = async () => {
-            // wait for chariot.onDonationRequest to be available as a function
-            while (typeof connect.onDonationRequest !== "function") {
-                console.log(
-                    "waiting for chariot.onDonationRequest to be available"
-                );
-                await new Promise((resolve) => setTimeout(resolve, 100));
-            }
-        }
-
-        loadConnect();
-
-        connect.onDonationRequest(async () => {
-            return {};
-        });
 
         return () => {
             document.body.removeChild(script);
